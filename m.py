6cbca5898f5c9b8a7282dd37d1926e706b34af41
Aravind Balaji <balajiar@amazon.com>
2019-08-29 18:42:51 +0530

    Removing postman headers and adding comments

--- m.py
+++ m.py
@@ -3,26 +3,23 @@ import json
 import sys
 
 url = "https://api.ratesapi.io/api/latest"
+
+#value of the base currency to be converted
 baseCurrency = sys.argv[2].upper()
+
+#base currency based on which the conversion should happen
 convertCurrency = sys.argv[3].upper()
 
 
 querystring = {"base":baseCurrency.upper()}
 
-headers = {
-    'Authorization': "Basic Og==",
-    'User-Agent': "PostmanRuntime/7.15.2",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "5a572626-e43b-4b65-9c61-933a6ef5ec59,31503acb-0844-44c3-9032-07cd3af9d757",
-    'Host': "api.ratesapi.io",
-    'Cookie': "__cfduid=d61a954e1902fdfe687f83565526959971566986551",
-    'Accept-Encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
+headers = {'Host': "api.ratesapi.io"}
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 jsonresp = json.loads(response.text)
+
+#Resultant currency that needs to be converted to
 arguements = sys.argv[1]
+
+#Multiply the resultant currency value with the base to display the current value
 print(jsonresp["rates"][convertCurrency] * float(arguements))
