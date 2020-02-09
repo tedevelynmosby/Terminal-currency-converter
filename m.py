ab0e5a7183529e4e3fd467ea0321effd38b6d593
Aravind Balaji <balajiar@amazon.com>
2020-02-09 23:53:28 +0530

    Modifying rest call with try catch blocks and printing pretty output.
    The change is required to try catch in case if service does not work
    or the currency input entered by the customer is wrong.

--- m.py
+++ m.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys
+import traceback
 
 url = "https://api.ratesapi.io/api/latest"
 
@@ -16,10 +17,17 @@ querystring = {"base":baseCurrency.upper()}
 headers = {'Host': "api.ratesapi.io"}
 
 response = requests.request("GET", url, headers=headers, params=querystring)
-jsonresp = json.loads(response.text)
-
-#Resultant currency that needs to be converted to
-arguements = sys.argv[1]
-
-#Multiply the resultant currency value with the base to display the current value
-print(jsonresp["rates"][convertCurrency] * float(arguements))
+try:
+    #Raise exception if url is status code is not 200.
+    response.raise_for_status()
+    jsonresp = json.loads(response.text)
+    #Resultant currency that needs to be converted to
+    arguements = sys.argv[1]
+    #Multiply the resultant currency value with the base to display the current value
+    convertedResponse = '{:.2f}'.format(jsonresp["rates"][convertCurrency] * float(arguements))
+    print(str(sys.argv[1] + " " + str(baseCurrency) + " to " + str(convertCurrency) +  " => " + str(convertedResponse)) + " " + str(convertCurrency).lower())
+    
+except:
+    print("\n===================\n BRUHception occured. You got BRUHed by the service or the currency you entered! Please check the stacktrace king. \n==================\n")
+    traceback.print_exc()
+    sys.exit(1)
